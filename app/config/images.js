10974e28f30e6142b19c2d73c679eff9800368e0
aperalta03 <aperalta03@tamu.edu>
2026-02-05 10:26:58 -0600

    New images

--- app/config/images.js
+++ app/config/images.js
@@ -1,3 +1,3 @@
 // Easy way to switch between image sets
 // Just change 'V1' to 'V2' (or vice versa) to switch image sets
-export const IMAGE_VERSION = 'V1'; // Change to 'V2' to use V2 images
+export const IMAGE_VERSION = 'V2'; // Change to 'V2' to use V2 images
